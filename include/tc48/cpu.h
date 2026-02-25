5f9695275193f6cd6e348735ff1a526cfb5a1828
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-02-25 20:05:11 +0100

    feat(tc48): add tc48_cpu initialization and basic operations

--- include/tc48/cpu.h
+++ include/tc48/cpu.h
@@ -1,5 +1,17 @@
 #pragma once
 
-typedef struct tc48_cpu {
+#include <stdint.h>
 
+struct tc48_system;
+typedef struct tc48_system tc48_system;
+
+typedef struct tc48_cpu {
+    tc48_system* sys;
 } tc48_cpu;
+
+void tc48_cpu_init(tc48_cpu* cpu);
+void tc48_cpu_deinit(tc48_cpu* cpu);
+
+void tc48_cpu_reset(tc48_cpu* cpu);
+void tc48_cpu_step(tc48_cpu* cpu);
+void tc48_cpu_run(tc48_cpu* cpu, uint64_t cycles);
---
+++ src/tc48/cpu.c
@@ -0,0 +1,8 @@
+#include <tc48/cpu.h>
+
+void tc48_cpu_init(tc48_cpu* cpu) { (void) cpu; }
+void tc48_cpu_deinit(tc48_cpu* cpu) { (void) cpu; }
+
+void tc48_cpu_reset(tc48_cpu* cpu) { (void) cpu; }
+void tc48_cpu_step(tc48_cpu* cpu) { (void) cpu; }
+void tc48_cpu_run(tc48_cpu* cpu, uint64_t cycles) { (void) cpu, (void) cycles; }
