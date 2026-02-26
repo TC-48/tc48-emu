6b03d2512073b4dfaf51d709e64ab028bf8ac3ba
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-02-26 10:23:13 +0100

    refactor(tc48/cpu/regs.h): rename and add read functions
    - Renamed tc48_cpu_set_zero_reg to tc48_cpu_zero_reg
    - Added new functions to read registers with different sizes

--- include/tc48/cpu/regs.h
+++ include/tc48/cpu/regs.h
@@ -15,5 +15,10 @@ typedef struct tc48_cpu_regs {
 } tc48_cpu_regs;
 
 void tc48_cpu_set_reg_imm(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst, tc48_imm* imm);
-void tc48_cpu_set_zero_reg(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst);
+void tc48_cpu_zero_reg(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst);
 void tc48_cpu_mov_reg(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst, tc48_reg_id src);
+
+tc48_tryte   tc48_cpu_read_reg6 (tc48_cpu_regs* regs, tc48_reg_id r);
+tc48_quarter tc48_cpu_read_reg12(tc48_cpu_regs* regs, tc48_reg_id r);
+tc48_half    tc48_cpu_read_reg24(tc48_cpu_regs* regs, tc48_reg_id r);
+tc48_word    tc48_cpu_read_reg48(tc48_cpu_regs* regs, tc48_reg_id r);
