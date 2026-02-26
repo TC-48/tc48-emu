0cb7f9190575b079936e0457192fcbab52c62b63
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-02-26 09:48:47 +0100

    refactor(tc48/cpu/regs.h): update register operations and add new function
    - Changed `tc48_cpu_set_reg_imm` to accept `tc48_doublet` width
    - Added `tc48_cpu_set_zero_reg` function
    - Updated `tc48_cpu_mov_reg` to accept `tc48_doublet` width

--- include/tc48/cpu/regs.h
+++ include/tc48/cpu/regs.h
@@ -14,5 +14,6 @@ typedef struct tc48_cpu_regs {
     tc48_word data[TC48_CPU_REGS_COUNT];
 } tc48_cpu_regs;
 
-void tc48_cpu_set_reg_imm(tc48_cpu_regs* regs, tc48_reg_id dst, tc48_imm* imm);
-void tc48_cpu_mov_reg(tc48_cpu_regs* regs, tc48_quarter width, tc48_reg_id dst, tc48_reg_id src);
+void tc48_cpu_set_reg_imm(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst, tc48_imm* imm);
+void tc48_cpu_set_zero_reg(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst);
+void tc48_cpu_mov_reg(tc48_cpu_regs* regs, tc48_doublet width, tc48_reg_id dst, tc48_reg_id src);
