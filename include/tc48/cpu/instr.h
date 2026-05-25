772c362e0efe0a6214abd6bd19d833df272861ad
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-05-25 08:56:13 +0200

    feat(instr): expand instruction formats and operand types

--- include/tc48/cpu/instr.h
+++ include/tc48/cpu/instr.h
@@ -5,11 +5,15 @@
 
 #include <tc48/word.h>
 
-enum tc48_instr_format: tc48_doublet {
+enum tc48_instr_format: tc48_triplet {
+    TC48_INSTR_FORMAT_NONE,
     TC48_INSTR_FORMAT_R,
+    TC48_INSTR_FORMAT_A,
+    TC48_INSTR_FORMAT_I,
     TC48_INSTR_FORMAT_RR,
     TC48_INSTR_FORMAT_RRR,
     TC48_INSTR_FORMAT_RI,
+    TC48_INSTR_FORMAT_RRI,
     TC48_INSTR_FORMAT_RA,
     TC48_INSTR_FORMAT_RRA,
 };
@@ -22,14 +26,20 @@ enum tc48_operand_width: tc48_doublet {
 };
 
 typedef struct tc48_instr {
-    tc48_doublet format;
+    tc48_triplet format;
     tc48_doublet width;
-    // 2 trits padding
+    // 1 trit padding
     tc48_tryte opcode;
     union {
         struct {
             tc48_reg_id r1;
         } r;
+        struct {
+            tc48_addr addr;
+        } a;
+        struct {
+            tc48_imm imm;
+        } i;
         struct {
             tc48_reg_id r1;
             tc48_reg_id r2;
@@ -43,6 +53,11 @@ typedef struct tc48_instr {
             tc48_reg_id r1;
             tc48_imm imm;
         } ri;
+        struct {
+            tc48_reg_id r1;
+            tc48_reg_id r2;
+            tc48_imm imm;
+        } rri;
         struct {
             tc48_reg_id r1;
             tc48_addr addr;
