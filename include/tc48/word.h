b1e4aa283db0774b81c0f0253bb487c61b6d53eb
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-05-25 08:27:09 +0200

    fix(word): rename BITS to TRITS and correct shift overflow handling

--- include/tc48/word.h
+++ include/tc48/word.h
@@ -6,60 +6,56 @@
 #include <tc48/defs.h>
 #include <tc48/pow3.h>
 
-typedef tc48_u8b  tc48_doublet;      /// 2 trits
-typedef tc48_u8b  tc48_triplet;      /// 3 trits
-typedef tc48_u8b  tc48_quadruplet;   /// 4 trits
-typedef tc48_u16b tc48_tryte;        /// 6 trits
-typedef tc48_u32b tc48_quarter;      /// 12 trits
-typedef tc48_u64b tc48_half;         /// 24 trits
-typedef tc48_u128b tc48_word;        /// 48 trits
+typedef tc48_u8b   tc48_doublet;      /// 2 trits
+typedef tc48_u8b   tc48_triplet;      /// 3 trits
+typedef tc48_u8b   tc48_quadruplet;   /// 4 trits
+typedef tc48_u16b  tc48_tryte;        /// 6 trits
+typedef tc48_u32b  tc48_quarter;      /// 12 trits
+typedef tc48_u64b  tc48_half;         /// 24 trits
+typedef tc48_u128b tc48_word;         /// 48 trits
 
 typedef tc48_word tc48_addr;
 
-/// Normal Binary Integer Representation: Value = sum(trit_i * 3^i)
-#define TC48_TRIT_BITS 2 // although we use integers, logically a trit needs ~1.58 bits, 2 bits for storage if packed. But here we just use the value.
+#define TC48_DOUBLET_TRITS    2
+#define TC48_TRIPLET_TRITS    3
+#define TC48_QUADRUPLET_TRITS 4
+#define TC48_TRYTE_TRITS      6
+#define TC48_QUARTER_TRITS    12
+#define TC48_HALF_TRITS       24
+#define TC48_WORD_TRITS       48
 
-#define TC48_DOUBLET_BITS    2
-#define TC48_TRIPLET_BITS    3
-#define TC48_QUADRUPLET_BITS 4
-#define TC48_TRYTE_BITS      6
-#define TC48_QUARTER_BITS    12
-#define TC48_HALF_BITS       24
-#define TC48_WORD_BITS       48
+#define TC48_DOUBLET_VALUES    (3ULL * 3ULL)                                /// 3^2
+#define TC48_TRIPLET_VALUES    (TC48_DOUBLET_VALUES * 3ULL)                 /// 3^3
+#define TC48_QUADRUPLET_VALUES (TC48_TRIPLET_VALUES * 3ULL)                 /// 3^4
+#define TC48_TRYTE_VALUES      (TC48_QUADRUPLET_VALUES * 9ULL)              /// 3^6
+#define TC48_QUARTER_VALUES    (TC48_TRYTE_VALUES * TC48_TRYTE_VALUES)      /// 3^12
+#define TC48_HALF_VALUES       (TC48_QUARTER_VALUES * TC48_QUARTER_VALUES)  /// 3^24
+#define TC48_WORD_VALUES       ((tc48_u128b)TC48_HALF_VALUES * TC48_HALF_VALUES)        /// 3^48
 
-#define TC48_DOUBLET_VALUES    (3ULL * 3ULL)                                     /* 3^2  */
-#define TC48_TRIPLET_VALUES    (TC48_DOUBLET_VALUES * 3ULL)                      /* 3^3  */
-#define TC48_QUADRUPLET_VALUES (TC48_TRIPLET_VALUES * 3ULL)                      /* 3^4  */
-#define TC48_TRYTE_VALUES      (TC48_QUADRUPLET_VALUES * 9ULL)                   /* 3^6  */
-#define TC48_QUARTER_VALUES    (TC48_TRYTE_VALUES * (tc48_u32b)TC48_TRYTE_VALUES)           /* 3^12 */
-#define TC48_HALF_VALUES       ((tc48_u64b)TC48_QUARTER_VALUES * TC48_QUARTER_VALUES)       /* 3^24 */
-#define TC48_WORD_VALUES       ((tc48_u128b)TC48_HALF_VALUES * TC48_HALF_VALUES) /* 3^48 */
-
-// Literal helpers
 #define _TC48_T(t, n) ((tc48_u128b)(t) * tc48_pow3_u128[n])
 
 #include <tc48/gen/word-lits.h>
 
-#define TC48_GEN_WORD_UTILS(NAME, TYPE, BITS, VALUES, U_BIN, I_BIN, SUFFIX, POW_TABLE)                  \
-   static inline tc48_trit_state tc48_##NAME##_get_trit(tc48_##NAME w, int n) {                         \
-       return (tc48_trit_state)((w / POW_TABLE[n]) % 3);                                               \
-   }                                                                                                    \
-   static inline void tc48_##NAME##_set_trit(tc48_##NAME *w, int n, tc48_trit_state t) {                \
+#define TC48_GEN_WORD_UTILS(NAME, TYPE, TRITS, VALUES, U_BIN, I_BIN, SUFFIX, POW_TABLE)                  \
+   static inline tc48_trit_state tc48_##NAME##_get_trit(tc48_##NAME w, int n) {                          \
+       return (tc48_trit_state)((w / POW_TABLE[n]) % 3);                                                 \
+   }                                                                                                     \
+   static inline void tc48_##NAME##_set_trit(tc48_##NAME *w, int n, tc48_trit_state t) {                 \
        *w = (TYPE)(*w - (tc48_##NAME##_get_trit(*w, n) * POW_TABLE[n]) + ((TYPE)t * POW_TABLE[n]));      \
-   }                                                                                                    \
-   static inline tc48_##NAME tc48_##NAME##_shift(tc48_##NAME w, int count) {                            \
-       if (count >= BITS || count <= -BITS) return 0;                                                   \
-       if (count >= 0) return (tc48_##NAME)((w * POW_TABLE[count]) % (VALUES));                          \
-       return (tc48_##NAME)(w / POW_TABLE[-count]);                                                     \
-   }                                                                                                    \
-   static inline U_BIN tc48_##NAME##_to_u##SUFFIX(tc48_##NAME w) {                                      \
-       return (U_BIN)w;                                                                                 \
-   }                                                                                                    \
-   static inline I_BIN tc48_##NAME##_to_i##SUFFIX(tc48_##NAME w) {                                      \
-       U_BIN u = (U_BIN)w;                                                                              \
-       U_BIN limit = (U_BIN)((VALUES) / 2);                                                             \
-       if (u > limit) return (I_BIN)(u - (U_BIN)(VALUES));                                              \
-       return (I_BIN)u;                                                                                 \
+   }                                                                                                     \
+   static inline tc48_##NAME tc48_##NAME##_shift(tc48_##NAME w, int count) {                             \
+       if (count >= TRITS || count <= -TRITS) return 0;                                                  \
+       if (count >= 0) return (tc48_##NAME)(((TYPE)w % (VALUES / POW_TABLE[count])) * POW_TABLE[count]); \
+       return (tc48_##NAME)(w / POW_TABLE[-count]);                                                      \
+   }                                                                                                     \
+   static inline U_BIN tc48_##NAME##_to_u##SUFFIX(tc48_##NAME w) {                                       \
+       return (U_BIN)w;                                                                                  \
+   }                                                                                                     \
+   static inline I_BIN tc48_##NAME##_to_i##SUFFIX(tc48_##NAME w) {                                       \
+       U_BIN u = (U_BIN)w;                                                                               \
+       U_BIN limit = (U_BIN)((VALUES) / 2);                                                              \
+       if (u > limit) return (I_BIN)(u - (U_BIN)(VALUES));                                               \
+       return (I_BIN)u;                                                                                  \
    }
 
 TC48_GEN_WORD_UTILS(doublet,    tc48_u8b,   2,  TC48_DOUBLET_VALUES,    tc48_u8b,   tc48_i8b,   8b,   tc48_pow3_u32)
--- tests/test-word.c
+++ tests/test-word.c
@@ -25,51 +25,59 @@ static tc48_i128b to_signed_ref(const tc48_trit_state *trits, int bits, tc48_u12
     return (tc48_i128b) u;
 }
 
-#define TEST_WORD(NAME, UNAME, BITS, VALUES, SUFFIX)                        \
-Test(word, NAME##_all) {                                                    \
-    tc48_trit_state trits[BITS];                                            \
-                                                                            \
-    for (int iter = 0; iter < 1000; ++iter) {                               \
-        tc48_##NAME v = 0;                                                  \
-                                                                            \
-        /* set/get */                                                       \
-        for (int i = 0; i < BITS; ++i) {                                    \
-            trits[i] = rand() % 3;                                          \
-            tc48_##NAME##_set_trit(&v, i, trits[i]);                        \
-            cr_assert_eq(tc48_##NAME##_get_trit(v, i), trits[i]);           \
-        }                                                                   \
-                                                                            \
-        /* to_u / to_i */                                                   \
-        cr_assert(tc48_##NAME##_to_u##SUFFIX(v) ==                          \
-                  to_unsigned_ref(trits, BITS));                            \
-                                                                            \
-        cr_assert(tc48_##NAME##_to_i##SUFFIX(v) ==                          \
-                  to_signed_ref(trits, BITS, VALUES));                      \
-                                                                            \
-        if (BITS > 1) {                                                     \
-            tc48_##NAME shl = tc48_##NAME##_shift(v, 1);                    \
-                                                                            \
-            cr_assert_eq(tc48_##NAME##_get_trit(shl, 0), TC48_ZERO);        \
-            for (int i = 1; i < BITS; ++i)                                  \
-                cr_assert(tc48_##NAME##_get_trit(shl, i) == trits[i - 1]);  \
-                                                                            \
-            tc48_##NAME shr = tc48_##NAME##_shift(v, -1);                   \
-                                                                            \
-            cr_assert_eq(tc48_##NAME##_get_trit(shr, BITS - 1), TC48_ZERO); \
-            for (int i = 0; i < BITS - 1; ++i)                              \
-                cr_assert(tc48_##NAME##_get_trit(shr, i) == trits[i + 1]);  \
-        }                                                                   \
-                                                                            \
-        /* overflow shift */                                                \
-        tc48_##NAME out = tc48_##NAME##_shift(v, BITS);                     \
-        cr_assert_eq(out, 0);                                               \
-    }                                                                       \
+#define TEST_WORD(NAME, UNAME, TRITS, VALUES, SUFFIX)                        \
+Test(word, NAME##_all) {                                                     \
+    tc48_trit_state trits[TRITS];                                            \
+                                                                             \
+    for (int iter = 0; iter < 1000; ++iter) {                                \
+        tc48_##NAME v = 0;                                                   \
+                                                                             \
+        /* set/get */                                                        \
+        for (int i = 0; i < TRITS; ++i) {                                    \
+            trits[i] = rand() % 3;                                           \
+            tc48_##NAME##_set_trit(&v, i, trits[i]);                         \
+            cr_assert_eq(tc48_##NAME##_get_trit(v, i), trits[i]);            \
+        }                                                                    \
+                                                                             \
+        /* to_u / to_i */                                                    \
+        cr_assert(tc48_##NAME##_to_u##SUFFIX(v) ==                           \
+                  to_unsigned_ref(trits, TRITS));                            \
+                                                                             \
+        cr_assert(tc48_##NAME##_to_i##SUFFIX(v) ==                           \
+                  to_signed_ref(trits, TRITS, VALUES));                      \
+                                                                             \
+        if (TRITS > 1) {                                                     \
+            tc48_##NAME shl = tc48_##NAME##_shift(v, 1);                     \
+                                                                             \
+            cr_assert_eq(tc48_##NAME##_get_trit(shl, 0), TC48_ZERO);         \
+            for (int i = 1; i < TRITS; ++i)                                  \
+                cr_assert(tc48_##NAME##_get_trit(shl, i) == trits[i - 1]);   \
+                                                                             \
+            tc48_##NAME shr = tc48_##NAME##_shift(v, -1);                    \
+                                                                             \
+            cr_assert_eq(tc48_##NAME##_get_trit(shr, TRITS - 1), TC48_ZERO); \
+            for (int i = 0; i < TRITS - 1; ++i)                              \
+                cr_assert(tc48_##NAME##_get_trit(shr, i) == trits[i + 1]);   \
+        }                                                                    \
+                                                                             \
+        /* overflow shift */                                                 \
+        tc48_##NAME out = tc48_##NAME##_shift(v, TRITS);                     \
+        cr_assert_eq(out, 0);                                                \
+    }                                                                        \
+}                                                                            \
+Test(word, NAME##_large_shift) {                                             \
+    tc48_##NAME v = 0;                                                       \
+    tc48_##NAME##_set_trit(&v, TRITS - 1, 1);                                \
+    for (int i = 1; i < TRITS; ++i) {                                        \
+        tc48_##NAME shl = tc48_##NAME##_shift(v, i);                         \
+        cr_assert_eq(shl, 0);                                                \
+    }                                                                        \
 }
 
-TEST_WORD(doublet,    DOUBLET,    TC48_DOUBLET_BITS,    TC48_DOUBLET_VALUES,    8b)
-TEST_WORD(triplet,    TRIPLET,    TC48_TRIPLET_BITS,    TC48_TRIPLET_VALUES,    8b)
-TEST_WORD(quadruplet, QUADRUPLET, TC48_QUADRUPLET_BITS, TC48_QUADRUPLET_VALUES, 8b)
-TEST_WORD(tryte,      TRYTE,      TC48_TRYTE_BITS,      TC48_TRYTE_VALUES,      16b)
-TEST_WORD(quarter,    QUARTER,    TC48_QUARTER_BITS,    TC48_QUARTER_VALUES,    32b)
-TEST_WORD(half,       HALF,       TC48_HALF_BITS,       TC48_HALF_VALUES,       64b)
-TEST_WORD(word,       WORD,       TC48_WORD_BITS,       TC48_WORD_VALUES,       128b)
+TEST_WORD(doublet,    DOUBLET,    TC48_DOUBLET_TRITS,    TC48_DOUBLET_VALUES,    8b)
+TEST_WORD(triplet,    TRIPLET,    TC48_TRIPLET_TRITS,    TC48_TRIPLET_VALUES,    8b)
+TEST_WORD(quadruplet, QUADRUPLET, TC48_QUADRUPLET_TRITS, TC48_QUADRUPLET_VALUES, 8b)
+TEST_WORD(tryte,      TRYTE,      TC48_TRYTE_TRITS,      TC48_TRYTE_VALUES,      16b)
+TEST_WORD(quarter,    QUARTER,    TC48_QUARTER_TRITS,    TC48_QUARTER_VALUES,    32b)
+TEST_WORD(half,       HALF,       TC48_HALF_TRITS,       TC48_HALF_VALUES,       64b)
+TEST_WORD(word,       WORD,       TC48_WORD_TRITS,       TC48_WORD_VALUES,       128b)
